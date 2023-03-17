05560c39eec6ba0eed06d1e963480df064613c96
mryinlc <mryinlc@qq.com>
2023-03-17 20:18:49 +0800

    初始化项目模块

---
+++ chatbot-api-interfaces/src/main/java/com/chatbot/ChatbotApiApplication.java
@@ -0,0 +1,11 @@
+package com.chatbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatbotApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ChatbotApiApplication.class, args);
+    }
+}
---
+++ chatbot-api-interfaces/src/test/java/com/chatbot/test/ApiTest.java
@@ -0,0 +1,7 @@
+package com.chatbot.test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ApiTest {
+}
